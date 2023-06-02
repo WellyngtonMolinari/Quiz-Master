fcc2296960cce54665bffe0a8e1e4ae2ca93568e
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-02 14:05:56 -0300

    Answer selector Update
    Created display question method and wrong answer funcionality

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -13,8 +13,44 @@ public class Quiz : MonoBehaviour
     int correctAnswerIndex;
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Sprite wrongAnswerSprite;
+    Image buttonImage;
 
     void Start()
+    {
+        DisplayQuestion();
+    }
+
+    // control when a button is selected
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            CorrectAnswer(index);
+        }
+        else
+        {
+            WrongAnswer(index);
+        }
+    }
+
+    private void CorrectAnswer(int index)
+    {
+        buttonImage = answerButtons[index].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
+    }
+
+    private void WrongAnswer(int index)
+    {
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+        string correctAnswer = question.GetAnswer(correctAnswerIndex);
+        questionText.text = "Wrong answer! The correct one is:\n " + correctAnswer;
+        buttonImage = answerButtons[index].GetComponent<Image>();
+        buttonImage.sprite = wrongAnswerSprite;
+    }
+
+    private void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -28,14 +64,4 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    // control when a button is pressed
-    public void OnAnswerSelected(int index)
-    {
-        if (index == question.GetCorrectAnswerIndex())
-        {
-            questionText.text = "Correct!";
-            Image buttonImage = answerButtons[index].GetComponent<Image>();
-            buttonImage.sprite = correctAnswerSprite;
-        }
-    }
 }
