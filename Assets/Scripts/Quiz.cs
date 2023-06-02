d7e214c6ca5d7f8d0c390d089971b2b847f8497c
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-02 12:16:34 -0300

    For loop added Update
    Added for loop to fill automaticaly the answers field based on array position of each one

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,10 +7,20 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    // array to store all answers buttons gameObjects 
+    [SerializeField] GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
-    }
 
+        // run the loop while i is less than the lenght of our answerButtons array
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            // get the children component of answer button gameObject and find the first textMeshPro component 
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            // get the answer at the array position
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
 }
